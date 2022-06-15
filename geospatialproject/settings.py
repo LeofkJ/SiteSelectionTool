1a84ca70b206b35c35fc2173b27863ab868bf118
clara-risk <43723728+clara-risk@users.noreply.github.com>
2022-06-15 14:46:04 -0400

    Update settings.py

--- geospatialproject/settings.py
+++ geospatialproject/settings.py
@@ -16,6 +16,8 @@ import os
 import os; os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geospatialproject.settings")
 from os import environ
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,12 +81,13 @@ WSGI_APPLICATION = 'geospatialproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
-
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
