2c23e3c027fd006b86b99035bd068a2899cd226c
clara-risk <43723728+clara-risk@users.noreply.github.com>
2022-06-15 14:50:49 -0400

    Update settings.py

--- geospatialproject/settings.py
+++ geospatialproject/settings.py
@@ -80,14 +80,18 @@ WSGI_APPLICATION = 'geospatialproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+# These lines make all the magic
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
