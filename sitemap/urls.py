d4f60cbd915d217bad21a4d0193197c60f6dbd1b
jourdanl <leo.jourdan@mail.utoronto.ca>
2022-08-17 16:27:01 -0400

    fix urls and delete unneeded code

--- sitemap/urls.py
+++ sitemap/urls.py
@@ -3,10 +3,4 @@ from .import views
 
 urlpatterns = [
     path('',views.get_pages, name='index'),
-    path('map/', views.map, name='map'),
-    path('map2/', views.map2, name='map2'),
-    path('map3/', views.map3, name='map3'),
-    path('map4/', views.map4, name='map4'),
-    path('map5/', views.map5, name='map5'),
-    path('map6/', views.map6, name='map6'),
 ]
